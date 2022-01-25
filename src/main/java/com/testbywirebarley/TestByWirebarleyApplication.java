51731e30c76e2db4f84edf4eb730bbb1bfa4108b
youngG209 <46043956+youngG209@users.noreply.github.com>
2022-01-25 11:19:05 +0900

    Initial commit

---
+++ src/main/java/com/testbywirebarley/TestByWirebarleyApplication.java
@@ -0,0 +1,13 @@
+package com.testbywirebarley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestByWirebarleyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestByWirebarleyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testbywirebarley/TestByWirebarleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testbywirebarley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestByWirebarleyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
